d1064ef68a5a083a4fc8078761a025a12df1d332
Erin H <e.hatherell@gmail.com>
2022-05-14 16:55:45 +1000

    Trying to fix big root-level error

--- client/src/App.js
+++ client/src/App.js
@@ -1,65 +1,25 @@
-// needs big work
-
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './components/pages/home';
-import Information from './components/pages/information';
-import Profile from './components/pages/profile';
-import NoMatch from './components/pages/noMatch';
-import Login from './components/pages/login';
-import Signup from './components/pages/signUp';
-import Profile from './components/pages/profile';
-import Rescues from './components/pages/rescues';
-import PortfolioContainer from "./components/PortfolioContainer";
-
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-
-
-
-        </div>
-        <Footer />
-      </div>
-    </ApolloProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
 export default App;
-
-
-
-
-const App = () => <PortfolioContainer />;
