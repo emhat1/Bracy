9e1d73e9887b7874ad88256178cb662b256f0df5
Erin H <e.hatherell@gmail.com>
2022-05-15 00:22:39 +1000

    Schemas and seeders replaced

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,22 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Message {
+    _id: ID
+    messageText: String
+    messageAuthor: String
+    createdAt: String
+  }
+
+  type Query {
+    messages: [Message]!
+    message(messageId: ID!): Message
+  }
+
+  type Mutation {
+    addMessage(messageText: String!, messageAuthor: String!): Message
+    removeMessage(MessageId: ID!): message
+  }
+`;
+
+module.exports = typeDefs;
