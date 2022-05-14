0327865ae7b7255d6010af9c64cc86ebf74d6985
Erin H <e.hatherell@gmail.com>
2022-05-15 01:58:08 +1000

    Working on message board

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,12 +1,43 @@
+// Importing external dependencies
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+// Importing internal dependencies
+import Auth from '../../utils/auth';
+
 
 const Header = () => {
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
   return (
     <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <h1 className="m-0">Squishy Faces</h1>
         <p className="m-0">The joy and pain of being created adorable</p>
       </div>
+      <div>
+          {Auth.loggedIn() ? (
+            <>
+              <Link className="btn btn-lg btn-info m-2" to="/profile">
+                {Auth.getProfile().data.username}'s profile
+              </Link>
+              <button className="btn btn-lg btn-light m-2" onClick={logout}>
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <Link className="btn btn-lg btn-info m-2" to="/login">
+                Login
+              </Link>
+              <Link className="btn btn-lg btn-light m-2" to="/signup">
+                Signup
+              </Link>
+            </>
+                     )}
+                     </div>
     </header>
   );
 };
---
+++ client/src/components/MessageForm/index.js
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+
+import { ADD_MESSAGE } from '../../utils/mutations';
+import { QUERY_MESSAGE } from '../../utils/queries';
+
+import Auth from '../../utils/auth';
+
+const MessageForm = () => {
+  const [messageText, setMessageText] = useState('');
+
+  const [characterCount, setCharacterCount] = useState(0);
+
+  const [addMessage, { error }] = useMutation(ADD_MESSAGE, {
+    update(cache, { data: { addMessage } }) {
+      try {
+        const { messages } = cache.readQuery({ query: QUERY_MESSAGE });
+
+        cache.writeQuery({
+          query: QUERY_MESSAGES,
+          data: { messages: [addMessage, ...messages] },
+        });
+      } catch (e) {
+        console.error(e);
+      }
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await addMessage({
+        variables: {
+          messageText,
+          messageAuthor: Auth.getProfile().data.username,
+        },
+      });
+
+      setMessageText('');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    if (name === 'messageText' && value.length <= 280) {
+      setMessageText(value);
+      setCharacterCount(value.length);
+    }
+  };
+
+  return (
+    <div>
+      <h3>What are your squishy-faced thoughts?</h3>
+
+      {Auth.loggedIn() ? (
+        <>
+          <p
+            className={`m-0 ${
+              characterCount === 280 || error ? 'text-danger' : ''
+            }`}
+          >
+            Character Count: {characterCount}/280
+          </p>
+          <form
+            className="flex-row justify-center justify-space-between-md align-center"
+            onSubmit={handleFormSubmit}
+          >
+            <div className="col-12 col-lg-9">
+              <textarea
+                name="messageText"
+                placeholder="Here's a new thought..."
+                value={thoughtText}
+                className="form-input w-100"
+                style={{ lineHeight: '1.5', resize: 'vertical' }}
+                onChange={handleChange}
+              ></textarea>
+            </div>
+
+            <div className="col-12 col-lg-3">
+              <button className="btn btn-primary btn-block py-3" type="submit">
+                Add Message
+              </button>
+            </div>
+            {error && (
+              <div className="col-12 my-3 bg-danger text-white p-3">
+                {error.message}
+              </div>
+            )}
+          </form>
+        </>
+      ) : (
+        <p>
+          You need to be logged in to share your thoughts. Please{' '}
+          <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default MessageForm;
