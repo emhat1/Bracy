4704dd85e1b09f8b0ccb0338870aca7996c0ddff
Erin H <e.hatherell@gmail.com>
2022-05-14 23:33:21 +1000

    Preparing for Stripe

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -5,7 +5,7 @@ const Header = () => {
     <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <h1 className="m-0">Squishy Faces</h1>
-        <p className="m-0">The joy and pain of being a cutie</p>
+        <p className="m-0">The joy and pain of being created adorable</p>
       </div>
     </header>
   );
