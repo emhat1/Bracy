5b8b9726f72fa3b40db434ac47fb7ccc883da9ab
Erin H <e.hatherell@gmail.com>
2022-05-15 14:35:06 +1000

    graphql queries fixed

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -8,16 +8,24 @@ const typeDefs = gql`
     createdAt: String
   }
 
+  type Rescue {
+    _id: ID
+    title: String
+    rescueType: String
+    suburb: String
+    state: String
+    website: String
+  }
+
   type Query {
     messages: [Message]!
     message(messageId: ID!): Message
-    
   }
 
   type Mutation {
     addMessage(messageText: String!, messageAuthor: String!): Message
     removeMessage(MessageId: ID!): Message
-    addRescue(title: String!, rescueType: String!, suburb: String!, state: String!, website: String!)
+    addRescue(title: String!, rescueType: String!, suburb: String!, state: String!, website: String!): Rescue
     removeRescue (RescueId: ID!): Rescue
   }
 `;
