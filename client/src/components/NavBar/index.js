af87be24220d293699f20a4f3185cff1842d0de2
Erin H <e.hatherell@gmail.com>
2022-05-16 11:43:52 +1000

    Logout added

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,73 +1,3 @@
-/* import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
-
-import Auth from '../../utils/auth';
-import { StyledNavbar } from './styles/Navbar.styled';
-
-export default function Navbar() {
-
-    const logout = (event) => {
-        event.preventDefault();
-        Auth.logout();
-    };
-
-    const [currentPage, setCurrentPage] = useState('Home');
-
-    const navbarPages = [
-                {
-            title: 'Dashboard',
-            link: '/dashboard',
-        },
-        {
-            title: 'Create Workout',
-            link: '/createWorkout',
-        },
-        {
-            title: 'View Workouts',
-            link: '/viewWorkouts',
-        },
-    ];
-
-    return (
-        <>
-            <StyledNavbar>
-                <ul className='navbarList'>
-                    <li key='home' className={currentPage === 'Home' ? 'navbarLinkActive' : 'navbarLink'}>
-                        <NavLink key='home' to='/' onClick={() => setCurrentPage('Home')}>
-                            Home
-                        </NavLink>
-                    </li>
-
-                    {navbarPages.map((page) => (
-                        Auth.loggedIn() ? (
-                            <li key={page.link} className={currentPage === page.title ? 'navbarLinkActive' : 'navbarLink'}>
-                                <NavLink key={page.link} to={page.link} onClick={() => setCurrentPage(page.title)} >
-                                    {page.title}
-                                </NavLink>
-                            </li>
-                        ) : (
-                            <li key={page.link}></li>
-                        )
-                    ))
-                    }
-                    <li key='login' className={currentPage === 'login' ? 'navbarLinkActive' : 'navbarLink'}>
-                        {Auth.loggedIn() ? (
-                            <NavLink to='/' onClick={logout}>
-                                Logout
-                            </NavLink>
-                        ) : (
-                            <NavLink key='login' to='/login' onClick={() => setCurrentPage('login')}>
-                                Login/Signup
-                            </NavLink>
-                        )}
-                    </li>
-                </ul>
-            </StyledNavbar>
-        </>
-    );
-}
-
- */
 // Importing external dependencies
 import React from 'react';
 import { NavLink } from 'react-router-dom';
@@ -77,6 +7,11 @@ import Auth from '../../utils/auth';
 
 
 function NavTabs({ currentPage, handlePageChange }) {
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
   return (
     
     <ul className="nav nav-tabs">
@@ -115,6 +50,16 @@ function NavTabs({ currentPage, handlePageChange }) {
           <li className="nav-item">
             <NavLink to="/Profile" className={( { isActive } ) => isActive ? 'nav-link active' : 'nav-link'}>
               Profile
+            </NavLink>
+          </li>
+        :
+          <></>
+      }
+      {
+        Auth.loggedIn() ?
+          <li className="nav-item">
+            <NavLink to='/' onClick={logout}>
+              Logout
             </NavLink>
           </li>
         :
